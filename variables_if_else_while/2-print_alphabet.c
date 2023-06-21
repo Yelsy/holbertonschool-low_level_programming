f7ca16647217445e386820563b54af45173d06a7
yelsy <yelsycg17@gmail.com>
2023-06-20 20:45:40 -0700

    printing alphabetic in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: "this file will print the
+ *  alphabetic in lowercase"
+ *  Return: Always 0 success
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+		putchar('\n');
+	}
+	return (0);
+}
