ca6bfe1393f727f0597eaf45a1e91adb97224eb5
yelsy <yelsycg17@gmail.com>
2023-06-20 21:24:45 -0700

    printing more numbers the other way

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: "this file will print the
+ * number from 0 to 9"
+ *  Return: Always 0 success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '10'; i++)
+	{
+		putchar(i);
+
+	}
+	putchar('\n');
+	return (0);
+}
