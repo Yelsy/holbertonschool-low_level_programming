fe06f42ea3adb411040053dfbc1f5965a8595719
yelsy <yelsycg17@gmail.com>
2023-06-22 13:01:19 -0700

    fix print-alphabet x10

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,12 +9,13 @@ void print_alphabet_x10(void)
 	char c;
 	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (i < 10)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
-			_putchar(i);
+			_putchar(c);
 		}
+		_putchar('\n');
+		i++;
 	}
-	_putchar('\n');
 }
