984ac30400754e5fbaf39e1071423f21e016d4ec
yelsy <yelsycg17@gmail.com>
2023-06-22 19:10:15 -0700

    printing the add of two numbers

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - prints the add of two integers
+ * @num1: number one
+ * @num2: number two
+ * Return: Add of number one and number two.
+ */
+
+int add(int num1, int num2)
+{
+	int result;
+
+	result = num1 + num2;
+	return (result);
+}
